35c93da801a7f8cc7b6c6d593401632d521e3c5d
Dan Train <dan.train@red-badger.com>
2022-12-06 17:28:18 +0000

    Add loader

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,35 @@
+import type { LoaderFunction } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+
+type Context = {
+  DB: D1Database;
+};
+
+export const loader: LoaderFunction = async ({ context }) => {
+  const ctx = context as Context;
+
+  const data = await ctx.DB.prepare("SELECT * FROM Customers").all();
+
+  return json({ data });
+};
+
 export default function Index() {
+  const data = useLoaderData();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        lineHeight: "1.4",
+        maxWidth: "1400px",
+        margin: "0 auto",
+      }}
+    >
+      <h1>Remix D1 Test</h1>
+      <pre style={{ background: "#eee", padding: "1em", overflow: "hidden" }}>
+        {JSON.stringify(data, null, 4)}
+      </pre>
     </div>
   );
 }
