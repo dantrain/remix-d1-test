81d1569a1621974221e8a9a5ad7ebb74adc1bb7b
Dan Train <dan.train@red-badger.com>
2022-12-17 17:13:33 +0000

    Switch to just a single table

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,14 +10,16 @@ export const loader: LoaderFunction = async ({ context }) => {
   const ctx = context as Context;
 
   const data = await ctx.DB.prepare(
-    // `SELECT Url, Title, Artist, ReleaseDate, Score, CriticNumber, Summary, RecordLabel, ImageUrl, GROUP_CONCAT(Name) Genres
-    // FROM Albums
-    // JOIN AlbumGenres ON AlbumGenres.AlbumUrl = Albums.Url
-    // JOIN Genres ON Genres.name = AlbumGenres.GenreName
-    // GROUP BY Url
-    // ORDER BY ReleaseDate DESC
-    // LIMIT 200`
-    `SELECT * FROM Albums LIMIT 200`
+    `SELECT * FROM Albums
+    WHERE
+      Score >= 80
+      AND CriticNumber >= 10
+      AND (
+        Genres LIKE '%,Indie Rock,%'
+        OR Genres LIKE '%,Rap,%'
+      )
+    ORDER BY ReleaseDate DESC
+    LIMIT 200`
   ).all();
 
   return json({ data });
--- seed/seed.mjs
+++ seed/seed.mjs
@@ -3,33 +3,13 @@
 /* eslint-disable no-undef */
 import chunk from "lodash/fp/chunk.js";
 import filter from "lodash/fp/filter.js";
-import flatten from "lodash/fp/flatten.js";
 import flow from "lodash/fp/flow.js";
 import map from "lodash/fp/map.js";
 import replace from "lodash/fp/replace.js";
-import sortBy from "lodash/fp/sortBy.js";
-import uniq from "lodash/fp/uniq.js";
 import { retry } from "zx/experimental";
 import "zx/globals";
 
 const albums = await fs.readJson("./seed/albums-17-12-22.json");
-
-const genreValues = flow(
-  map("genres"),
-  flatten,
-  uniq,
-  filter((_) => _),
-  filter((_) => !/([a-z]|R&B)[A-Z]/.test(_)),
-  sortBy((_) => _),
-  map(replace(/'/g, "''")), // Escape single quotes for Sqlite
-  map((_) => `('${_}')`)
-)(albums).join(",");
-
-const genresInsertCommand = `INSERT OR REPLACE INTO Genres (Name) VALUES ${genreValues}`;
-await retry(
-  10,
-  () => $`wrangler d1 execute metadb --command ${genresInsertCommand} --local`
-);
 
 const albumValuesChunked = chunk(200)(
   map(
@@ -51,6 +31,11 @@ const albumValuesChunked = chunk(200)(
           _.summary,
           _.record_label,
           _.image_url,
+          `,${flow(
+            filter((_) => _),
+            filter((_) => !/([a-z]|R&B)[A-Z]/.test(_)),
+            map(replace(/'/g, "''")) // Escape single quotes for Sqlite
+          )(_.genres).join(",")},`,
         ]),
       (_) => `(${_.join(",")})`
     )
@@ -59,35 +44,10 @@ const albumValuesChunked = chunk(200)(
 
 for (const chunk of albumValuesChunked) {
   const albumValues = chunk.join(",");
-  const albumsInsertCommand = `INSERT OR REPLACE INTO Albums (Url,Title,Artist,ReleaseDate,Score,CriticNumber,Summary,RecordLabel,ImageUrl) VALUES ${albumValues}`;
+  const albumsInsertCommand = `INSERT OR REPLACE INTO Albums (Url,Title,Artist,ReleaseDate,Score,CriticNumber,Summary,RecordLabel,ImageUrl,Genres) VALUES ${albumValues}`;
   await retry(
     10,
-    () => $`wrangler d1 execute metadb --command ${albumsInsertCommand} --local`
-  );
-}
-
-const albumGenresValuesChunked = chunk(200)(
-  flow(
-    map((album) =>
-      flow(
-        filter((_) => _),
-        filter((_) => !/([a-z]|R&B)[A-Z]/.test(_)),
-        map(replace(/'/g, "''")), // Escape single quotes for Sqlite
-        map((genre) => [album.url, genre])
-      )(album.genres)
-    ),
-    flatten,
-    map((_) => `(${map((_) => `'${_}'`)(_).join(",")})`)
-  )(albums)
-);
-
-for (const chunk of albumGenresValuesChunked) {
-  const albumGenresValues = chunk.join(",");
-  const albumGenresInsertCommand = `INSERT OR REPLACE INTO AlbumGenres (AlbumUrl,GenreName) VALUES ${albumGenresValues}`;
-  await retry(
-    10,
-    () =>
-      $`wrangler d1 execute metadb --command ${albumGenresInsertCommand} --local`
+    () => $`wrangler d1 execute metadb --command ${albumsInsertCommand}`
   );
 }
 
